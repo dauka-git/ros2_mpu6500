931e1af031592383af5024f5a349f9484fc32f10
dauka-git <marlambekovdaulet@gmail.com>
2025-12-28 20:25:41 +0500

    HELL NAHH

--- src/mpu6500_node.cpp
+++ src/mpu6500_node.cpp
@@ -2,6 +2,7 @@
 
 #include <chrono>
 #include <memory>
+#include <cmath>
 
 using namespace std::chrono_literals;
 
@@ -32,6 +33,9 @@ Mpu6500Node::Mpu6500Node(const std::string& name)
 
 void Mpu6500Node::ImuPubCallback()
 {
+    const double GRAVITY = 9.80665;
+    const double DEG_TO_RAD = M_PI / 180.0;
+
     auto message = sensor_msgs::msg::Imu();
     message.header.stamp = this->get_clock()->now();
     message.header.frame_id = "base_link";
@@ -51,13 +55,17 @@ void Mpu6500Node::ImuPubCallback()
         return;
     }
 
-    message.linear_acceleration.x = AccelData.Accel_X - accel_x_offset_;
-    message.linear_acceleration.y = AccelData.Accel_Y - accel_y_offset_;
-    message.linear_acceleration.z = AccelData.Accel_Z - accel_z_offset_;
+    message.linear_acceleration.x = (AccelData.Accel_X - accel_x_offset_) * GRAVITY;
+    message.linear_acceleration.y = (AccelData.Accel_Y - accel_y_offset_) * GRAVITY;
+    message.linear_acceleration.z = (AccelData.Accel_Z - accel_z_offset_) * GRAVITY;
     message.angular_velocity_covariance[0] = {0};
-    message.angular_velocity.x = GyroData.Gyro_X - gyro_x_offset_;
-    message.angular_velocity.y = GyroData.Gyro_Y - gyro_y_offset_;
-    message.angular_velocity.z = GyroData.Gyro_Z - gyro_z_offset_;
+    message.angular_velocity.x = (GyroData.Gyro_X - gyro_x_offset_) * DEG_TO_RAD;
+    message.angular_velocity.y = (GyroData.Gyro_Y - gyro_y_offset_) * DEG_TO_RAD;
+    message.angular_velocity.z = (GyroData.Gyro_Z - gyro_z_offset_) * DEG_TO_RAD;
+
+    RCLCPP_INFO(this->get_logger(), "Accel: x=%.3f, y=%.3f, z=%.3f | Gyro: x=%.3f, y=%.3f, z=%.3f",
+                AccelData.Accel_X, AccelData.Accel_Y, AccelData.Accel_Z,
+                GyroData.Gyro_X, GyroData.Gyro_Y, GyroData.Gyro_Z);
 
     message.orientation_covariance[0] = -1;
     message.orientation.x = 0;
